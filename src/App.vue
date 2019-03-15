8e53abff17b275c62f3b7904384fe0f2da5317c7
lisiyuan <1402740958@qq.com>
2019-03-15 15:55:15 +0800

    路由

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-
+      <Xfooter></Xfooter>
   </div>
 </template>
 
 <script>
-
-
+import Vue from "vue"
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+import Xfooter from "./components/Xfooter.vue"
 export default {
   components: {
-
+    Xfooter
   }
 }
 </script>
---
+++ src/components/Xfooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <van-tabbar v-model="active" active-color="#ff6700">
+    <van-tabbar-item v-for="(page,idx) in pages" :key="idx" :icon="page.icon" >标签</van-tabbar-item>
+  </van-tabbar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+      pages: [
+        {
+          text: "首页",
+          icon: "star",
+          path: "/home",
+          name: "Home"
+        },
+        {
+          text: "分类",
+          icon: "balance-list",
+          path: "/tap",
+          name: "Tap"
+        },
+        {
+          text: "发现",
+          icon: "like",
+          path: "/find",
+          name: "Find"
+        },
+        {
+          text: "购物车",
+          icon: "shopping-cart",
+          path: "/cart",
+          name: "Cart"
+        },
+        {
+          text: "我的",
+          icon: "friends",
+          path: "/mine",
+          name:"Mine"
+        }
+      ]
+    };
+  }
+};
+</script>
+
