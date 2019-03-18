e622a15470f25e666c58a31094822f1c1b88c61a
lisiyuan <1402740958@qq.com>
2019-03-18 20:21:02 +0800

    冲突解决完毕

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,11 @@
 import Vue from "vue"
 import Vant from 'vant';
 import 'vant/lib/index.css';
-Vue.use(Vant);
-
-
-
-
-
-import Xfooter from "./components/Xfooter.vue"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
-
+Vue.use(Vant);
 import Xfooter from "./components/Xfooter.vue";
-
 import axios from 'axios';
 Vue.prototype.$axios = axios;
 
